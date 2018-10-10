8a6ba3c1eddab470e1db465f81503d94f8ee2f37
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 14:57:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,7 @@ end
 def batch_badge_creator(array)
   array.collect {|name| badge_maker(name)}
 end
+
+def assign_rooms(array)
+  array.collect {|array| "Hello, #{array}! You'll be assigned to room #{each_with_index + 1}!"}
+end
